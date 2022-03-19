ef11b9be1e5e385823f9ddebc30822e21d3ff73e
Sam Neale <sammneale@icloud.com>
2022-03-19 21:44:34 +1100

    restart

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './src/assets/index.js',
-    output: {
-        filename: 'assets/index.js',
-        path: path.resolve(__dirname, 'output'),
-    },
-};
