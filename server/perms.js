87e249af5124ef2c4ee6bc9f9a57a44c235cc870
Arman Danesh <armand@dev1.shayda.org>
2022-04-06 12:17:57 +0000

    Simple perms module -- initial version

---
+++ server/perms.js
@@ -0,0 +1,64 @@
+/*****
+ * 
+ * Require mandatory modules/packages
+ * 
+ */
+
+// Modules for interacting with filesyste
+const fs = require('fs');
+const path = require('path');
+
+/*****
+ * 
+ * Import permissions schema
+ * 
+ */
+
+const perms = JSON.parse(fs.readFileSync(path.join(__dirname, 'perms.json')));
+
+/*****
+ * 
+ * Create module constructor
+ */
+function Perm(userPerms = 0) {
+
+
+	// Check if a user has a perm
+	this.has = (perm) => {
+		return ((userPerms & perms[perm]) == perms[perm])
+	}
+
+	// Set a perm
+	this.set = (perm) => {
+		userPerms = userPerms | perms[perm];
+	}
+
+	// Remove a perm
+	this.unset = (perm) => {
+		userPerms = userPerms & (~ perms[perm]);
+	}
+
+	// Get list of all perms for the user
+	this.get = () => {
+
+		let permList = {};
+
+		for (perm in perms) {
+			permList[perm] = this.has(perm);
+		}
+
+		return permList;
+
+	}
+
+	// Get the numeric value of the perms for the user for storing in DB etc
+	this.value = () => {
+		return userPerms;
+	}
+
+}
+
+module.exports = {
+	Perm: Perm,
+	schema: Object.keys(perms)
+}
